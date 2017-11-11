72cc4f8a783d79f21d8a4169736ddfe925cc5b41
Hannes Andersson <hannes.andersson87@gmail.com>
2017-11-11 16:25:43 +0100

    Renamed the data access files

--- Stonebuck/Models/DatabaseAccess/NewsAppDataAccess.cs
+++ Stonebuck/Models/DatabaseAccess/NewsAppDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,19 @@ namespace Stonebuck.Models.DatabaseAccess
 {
     public class NewsAppDataAccess : INewsAppDataAccess
     {
+        IMongoClient _client;
+        IMongoDatabase _db;
+
+        const string NEWSAPP_DATABASE_NAME = "NewsAppDB";
+
+        public NewsAppDataAccess()
+        {
+            _client = new MongoClient(); // TODO send in connection string here
+            _db = _client.GetDatabase(NEWSAPP_DATABASE_NAME);
+        }
+
+
+
         public Task<IEnumerable<ISubscribable>> GetAllSubscribables()
         {
             return Task.Run(() => GetExampleSubscriptions());
